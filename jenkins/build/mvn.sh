200c84caa160f3f1881d4ecefcc5ad6b41d25a06
ricardo <alaya.hamza@gmail.com>
2020-05-01 21:02:36 +0200

    Adds docker config

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "*************************"
 echo "*****Building jar********"
 echo "*************************"
 
-WORKSPACE=/var/lib/jenkins/workspace/spring-angular-docker-app/spring-boot-client
+WORKSPACE=/var/lib/jenkins/workspace/spring-angular-docker-app/spring-boot-app
 
 docker run --rm -v $WORKSPACE:/app -v /root/.m2 -w /app maven-npm "$@"
